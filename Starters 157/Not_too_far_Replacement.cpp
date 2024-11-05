ab54f7764685172c8ba06618de5af78a6392a364
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-11-06 01:09:00 +0530

    pushes

---
+++ Starters 157/Not_too_far_Replacement.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+// order_of_key(k) : no. of elements < k
+// *find_by_order(i) : value at index i (0-based)
+
+#define int long long int
+#define ll long long
+#define ld long double 
+#define fastIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout.precision(numeric_limits<double>::max_digits10);
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define memset(arr,val) memset(arr,val,sizeof(arr))
+#define INF INT64_MAX
+#define pb push_back
+#define pii pair<int,int>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define sti stack<int>
+#define pop st.pop();
+#define top st.top();
+#define push(a) st.push(a);
+#define given                   \
+    int n;                      \
+    cin >> n;
+#define take(a, n)              \
+    for (int j = 0; j < n; j++) \
+        cin >> a[j];
+#define show(a, n)              \
+    for (int j = 0; j < n; j++) \
+        cout << a[j] << ' ';    \
+    cout << '\n';
+#define all(x) x.begin(), x.end()
+#define rep(i,x,y) for(int i=x; i<y; i++)
+#define rrep(i,x,y) for(int i=x; i>=y; i--)
+#define setbits(n)  __builtin_popcountll(n)
+#define bitsize(n) (63 - __builtin_clzll(n))
+#define lcm(a,b) (a/__gcd(a, b)*b)
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+// #define MOD1 1000000007
+#define MOD1 998244353
+#define nl '\n'
+#define PI 3.141592653589793238462
+
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+void merge(vector<int>& arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while ((left <= mid) && (right <= high))
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high) 
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int>& arr, int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+void solve() {
+
+    given
+    vi a(n);
+    int s;
+    take(a,n)
+    cin >> s;
+    mergeSort(a, 0, n-1);
+    for(int i = 0; i<n; i++){
+        if(a[i] <= 2*s && a[i]>s){
+            int temp = a[i];
+            a[i] = s;
+            s = temp;
+        }
+    }
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        sum = sum + a[i];
+    }
+    cout << sum << nl;
+    return;
+}
+
+signed main() {
+
+    fastIO;
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
